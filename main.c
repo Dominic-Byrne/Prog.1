687be94c4f8875a2c30e8664149dd2e76e88d1ca
Dominic Byrne <c3330676@uon.edu.au>
2019-04-02 23:28:41 +0000

    first template for encryption

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
-
+void encrypt
 int main()
 {
-    printf("Hello World");
-
+    char message[100], letter;   // message  is the string  
+    int number , key = 3;    // key is the number of revolutions, while the number should hold the results  
+    char message[] = "ATTACK OF THE CLONES"; // message to be encrypted 
+    for(number = 0; message[number] != NULL; number++) {
+         // this says that while there are still letters in message to contiune till it finds the NULL point 
+        if(letter >= 'a' && letter <= 'z') {
+            letter = (letter + key)(%26); 
+           if (letter)
+            
+            
+            message[number] = letter 
+            
+    
+    
     return 0;
 }
