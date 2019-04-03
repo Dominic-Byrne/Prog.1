3ca4b11dce952ab987a6a123918b1f35f4a4f7e5
Dominic Byrne <c3330676@uon.edu.au>
2019-04-03 00:27:08 +0000

    super confused attempt at creating a function

--- main.c
+++ main.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
-void encrypt
+void encrypt(char encryted, int rotationAmount); // function of encyrtion 
 int main()
 {
-    char message[100], letter;   // message  is the string  
-    int number , key = 3;    // key is the number of revolutions, while the number should hold the results  
-    char message[] = "ATTACK OF THE CLONES"; // message to be encrypted 
-    for(number = 0; message[number] != NULL; number++) {
-         // this says that while there are still letters in message to contiune till it finds the NULL point 
-        if(letter >= 'a' && letter <= 'z') {
-            letter = (letter + key)(%26); 
-           if (letter)
-            
-            
-            message[number] = letter 
-            
+       
+    int key = 3;    // key is the number of revolutions, 
+    char message[100] = "ATTACK OF THE CLONES"; // message to be encrypted 
+    
+    encrypt(message, key); // calls function 
+    
+    printf( "Encryted message: %s", message );
+             
     
     
     return 0;
 }
+void encrypt(char encryted, int rotationAmount)
+{
+    char letter;
+    int number = 0;
+    
+    
+    while (encryted != NULL)
+    {// this says that while there are still letters in message to contiune till it finds the NULL point 
+         
+        if(letter >= 'a' && letter <= 'z') 
+        {
+            letter = (letter + rotationAmount)%26; 
+
+            message[number] = letter; 
+        }
+        else if(letter >= 'A' && letter <= 'Z') 
+        {
+            letter = (letter + rotationAmount)%26; 
+
+            message[number] = letter;
+        }
+        number++;
+    }
+
+    
+}
+
+   
