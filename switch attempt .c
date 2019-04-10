af83f0ea4edaefec4fc0ec41351157e1d672d780
Dominic Byrne <c3330676@uon.edu.au>
2019-04-10 00:41:33 +0000

    decrypts and encrypts one word

--- switch attempt .c
+++ switch attempt .c
@@ -5,25 +5,29 @@ int main(void)
     int x, i, key;
     char message[100];
     
-    printf("Enter message to be encrypted or decrypted in capitals: /n");
+    printf("Enter message to be encrypted or decrypted in capitals: ");
     scanf("%s", &message);
-    printf("Enter key as an integer: /n");
+    printf("Enter key as an integer: ");
     scanf("%d", &key);
-    printf("If code needs to be encrypted enter 1/n If code needs to be decrypted enter 2/n Select 1 or 2: /n");
+    printf("If code needs to be encrypted enter 1\n"); 
+    printf("If code needs to be decrypted enter 2\n"); 
+    printf("Select 1 or 2: ");
     scanf("%d", &x);
     
-    switch(x)
-        case 1:
-        while (message[i] ! = 0)
-            message = message + key;
-            printf("Encrypted message is: %s/n", message);
+    switch(x){
+    case 1:
+        for (i=0;(message[i]!=0);i++)
+            message[i] = message[i] + key;
+            printf("Encrypted message is: %s\n", message);
             break;
-        case 2: 
-        while (message[i] != 0)
-            message = message + key;
-            printf("Decrypted message is: %s/n", message);
+    case 2: 
+        for (i=0;(message[i]!=0);i++)
+            message[i] = message[i] + key;
+            printf("Decrypted message is: %s\n", message);
             break;
-        default:
+    default:
             printf("ERROR");
-            
+        
 }
+}
+           
