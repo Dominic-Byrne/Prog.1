bb46ececd4822a9c58f26dd42b031d9415f6b6d3
Dominic Byrne <c3330676@uon.edu.au>
2019-04-28 07:40:14 +0000

    still trying

---
+++ EncryptCaesarKey.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void DecryptCaesar(char ,int ); // FUNCTION PROTOTYPE 
+int main(void)
+{
+int number = 1;
+char statment[100] = "ATTACK OF THE CLONE"; 
+
+DecryptCaesar(stament[100],number);
+
+return 0;
+   
+}
+
+{
+void DecryptCaesar(char message, int key) // the function definition 
+
+int decrypt; // variable for the switch statement  
+int i;
+for (i=0;i<100;i++){ // for loop the function 
+if (message[i]>65 && message[i]<90)
+    decrypt = 1; // if the ASCII value is between the captials decrypt = 1 
+else
+    decrypt = 0; // otherwise decrypt= 0 e.g whitespace or a symbol 
+switch(decrypt){
+    case 1:
+        message[i] = (message[i] - key);
+        if(message[i] < 90){
+            message[i] = (message[i] - 26);
+        }
+        break; 
+    case 0: 
+        break; // this is to do nothing as it is whitespace 
+        }
+ }       
+ puts(message);
+}
