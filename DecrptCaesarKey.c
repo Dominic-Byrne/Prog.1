88d52cada913a18573c339fdf9ff83f4babf584b
Dominic Byrne <c3330676@uon.edu.au>
2019-04-29 00:39:14 +0000

    EncryptCaesarKey.c and DecryptCaesarKey.c work

--- DecrptCaesarKey.c
+++ DecrptCaesarKey.c
@@ -6,25 +6,25 @@ int main(void)
 int number = 1;
 char statment[100] = "ABCD"; 
 
-EncryptCaesar(statment,number); // calls the function below up into the main fuction
+DecryptCaesar(statment,number); // calls the function below up into the main fuction
 
 return 0;
    
 }
 
 
-void EncryptCaesar(char* message, int key) // the function definition 
+void DecryptCaesar(char* message, int key) // the function definition 
 {
-int encrypt; // variable for the switch statement  
+int decrypt; // variable for the switch statement  
 int i;
 for (i=0;i<100;i++){ // for loop the function 
 if (message[i]>65 && message[i]<90)
-    encrypt = 1; // if the ASCII value is between the captials encrypt = 1 
+    decrypt = 1; // if the ASCII value is between the captials encrypt = 1 
 else
-    encrypt = 0; // otherwise encrypt= 0 e.g whitespace or a symbol 
-switch(encrypt){
+    decrypt = 0; // otherwise decrypt= 0 e.g whitespace or a symbol 
+switch(decrypt){
     case 1:
-        message[i] = (message[i] + key);
+        message[i] = (message[i] - key);
         if(message[i] > 90){
             message[i] = (message[i] - 26);
         }
