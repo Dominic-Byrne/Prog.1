8959dd4364b48a23cb9749fca8a09f9583eae2da
Dominic Byrne <c3330676@uon.edu.au>
2019-04-29 00:23:28 +0000

    only main.c works to encrypt and decrypt Caesar cipher with a key and comment tell how to switch between decrypting and encrypting

--- EncryptCaesarKey.c
+++ EncryptCaesarKey.c
@@ -5,7 +5,7 @@ int main(void)
 int number = 1;
 char statment[100] = "ATTACK OF THE CLONE"; 
 
-DecryptCaesar(stament[100],number);
+DecryptCaesar(statment,number); // calls the function below up into the main fuction
 
 return 0;
    
@@ -18,12 +18,12 @@ int decrypt; // variable for the switch statement
 int i;
 for (i=0;i<100;i++){ // for loop the function 
 if (message[i]>65 && message[i]<90)
-    decrypt = 1; // if the ASCII value is between the captials decrypt = 1 
+    decrypt = 1; // if the ASCII value is between the captials encrypt = 1 
 else
-    decrypt = 0; // otherwise decrypt= 0 e.g whitespace or a symbol 
+    decrypt = 0; // otherwise encrypt= 0 e.g whitespace or a symbol 
 switch(decrypt){
     case 1:
-        message[i] = (message[i] - key);
+        message[i] = (message[i] + key);
         if(message[i] < 90){
             message[i] = (message[i] - 26);
         }
--- main.c
+++ main.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
 int main(void)
 {
-    int x, i, key;
-    char message[100];
-    
-    printf("Enter message to be encrypted or decrypted in capitals: ");
-    scanf("%s", &message);
-    printf("Enter key as an integer: ");
-    scanf("%d", &key);
-    printf("If code needs to be encrypted enter 1\n"); 
-    printf("If code needs to be decrypted enter 2\n"); 
-    printf("Select 1 or 2: ");
-    scanf("%d", &x);
+    int x = 1, i, key 4; // change x to 1 for encryption or 2 of decryption 
+    char message[100] = "CLONE";// imputed string must be in capitals 
     
     switch(x){
     case 1:
-        for (i=0;(message[i]!=0);i++){
-            if (message[i]>65 && message[i]<90){
+        for (i=0;(message[i]!=0);i++){ // loop for the entire string 
+            if (message[i]>65 && message[i]<90){ // if the value is between the ASCII values of the capital letters 
             message[i] = (message[i] + key);
             if((message[i]+key) > 90){
-            message[i] = (message[i] + (key - 27));
+            message[i] = (message[i] - 26);
             }}
-        }
+        } // if the value encrypted is greater then the ASCII value of Z or 90 then 26 is minused to make it a capital letter 
             printf("Encrypted message is: %s", message);
             
             break;
@@ -30,9 +21,9 @@ int main(void)
             if (message[i]>65 && message[i]<90){
             message[i] = (message[i] + key);
             if((message[i]+key) > 90){
-            message[i] = (message[i] + (key - 27));
+            message[i] = (message[i] -26);
             }}
-        }
+        }// if the value decrypted is greater then the ASCII value of Z or 90 then 26 is minused to make it a capital letter 
             printf("Decrypted message is: %s", message);
             
             break;
